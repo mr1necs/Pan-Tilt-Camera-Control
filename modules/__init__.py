a8adbffcf9bf323a6e9928567c8bf863d994ae83
Maks Mr1necs <46063054+mr1necs@users.noreply.github.com>
2025-08-05 22:12:24 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,15 @@
 # modules/__init__.py
 
-from .yolo_detection import YOLODetector
 from .hsv_detection import HSVDetector
+from .object_tracker import ObjectTracker
 from .video_capture import VideoCapture
+from .yolo_detection import YOLODetector
 from .motor_controller import MotorController
 
 __all__ = [
     "YOLODetector",
     "HSVDetector",
     "VideoCapture",
-    "MotorController",
+    "ObjectTracker",
+    "MotorController"
 ]
